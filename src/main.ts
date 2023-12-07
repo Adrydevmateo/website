08cf62aca0961638c72ae854963db547e5f12e4b
AdryDev <adrydevmateo@gmail.com>
2023-12-07 00:03:47 -0400

    Setting up the project

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createHead } from '@unhead/vue'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+const head = createHead()
+
+app.use(createPinia())
+app.use(router)
+app.use(head)
+
+app.mount('#app')
---
+++ src/views/Home-View.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useMainStore } from "@/stores/main"
+import { useHead } from "@unhead/vue"
+
+const mainStore = useMainStore()
+
+const title: { [key: string]: string } = {
+  en: "Home Page",
+  es: "Pagina de Inicio"
+}
+
+useHead({
+  title: () => title[mainStore.CurrentLanguage]
+})
+</script>
+
+<template>
+  <main>
+    <h1>Home Page in: {{ mainStore.CurrentLanguage }}</h1>
+    <button @click="mainStore.ChangeLanguage('en')">English</button>
+    <button @click="mainStore.ChangeLanguage('es')">Spanish</button>
+  </main>
+</template>
