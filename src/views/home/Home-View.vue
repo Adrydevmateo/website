13dab06ece452ec42a0f6d70b3e91bb9af1ea41a
AdryDev <adrydevmateo@gmail.com>
2023-12-12 08:22:37 -0400

    added a row of technologies to Home - Presentation

--- src/views/home/Home-View.vue
+++ src/views/home/Home-View.vue
@@ -2,27 +2,61 @@
 import { useMainStore } from '@/stores/main.store'
 import { useHead } from '@unhead/vue'
 import homeTranslations from './Home.translations'
+import IconVue from '../../assets/icons/technologies/LogosVue.svg'
+import IconTypeScript from '../../assets/icons/technologies/LogosTypescriptIcon.svg'
+import IconNodejs from '../../assets/icons/technologies/LogosNodejs.svg'
+import IconDocker from '../../assets/icons/technologies/LogosDockerIcon.svg'
 
 const mainStore = useMainStore()
 
 useHead({
   title: () => homeTranslations.pageTitle[mainStore.CurrentLanguage]
 })
+
+const collectionOfTechnologies = [
+  {
+    name: 'ts',
+    icon: IconTypeScript
+  },
+  {
+    name: 'vue',
+    icon: IconVue
+  },
+  {
+    name: 'nodejs',
+    icon: IconNodejs
+  },
+  {
+    name: 'docker',
+    icon: IconDocker
+  }
+]
 </script>
 
 <template>
   <main id="home-view">
     <section id="home-presentation" class="home-container">
-      <h1
-        class="home-title heading heading-1"
-        v-html="homeTranslations.title[mainStore.CurrentLanguage]"
-      ></h1>
-      <p
-        class="base-font-size-text text-line-height"
-        v-html="homeTranslations.presentationText[mainStore.CurrentLanguage]"
-      ></p>
+      <div>
+        <h1
+          class="home-title heading heading-1"
+          v-html="homeTranslations.title[mainStore.CurrentLanguage]"
+        ></h1>
+        <p
+          class="base-font-size-text text-line-height"
+          v-html="homeTranslations.presentationText[mainStore.CurrentLanguage]"
+        ></p>
+      </div>
 
       <!-- TODO: LIST OF TECHNOLOGIES -->
+      <ul class="home-presentation--list-of-technologies">
+        <li
+          class="home-presentation--list-of-technologies--technology"
+          v-for="technology in collectionOfTechnologies"
+          :key="technology.name"
+        >
+          <img :src="technology.icon" :alt="`Icon of technology: ${technology.name}`" />
+        </li>
+      </ul>
     </section>
 
     <section id="home-about" class="home-container">
@@ -40,9 +74,32 @@ useHead({
 </template>
 
 <style scoped>
-/* PRESENTATION */
 #home-view {
   margin: 0rem 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 4em;
+}
+
+/* PRESENTATION */
+#home-presentation {
+  display: flex;
+  flex-direction: column;
+  gap: 4em;
+}
+
+.home-presentation--list-of-technologies {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 1em;
+}
+
+.home-presentation--list-of-technologies--technology > img {
+  width: 100%;
+  height: 6em;
 }
 
 /* ABOUT */
