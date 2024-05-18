9b4893a46b9642d4507bf5302cddeb4520c2dcfa
AdryDev <adrydevmateo@gmail.com>
2024-05-18 10:16:13 -0400

    React router dom added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Routes, Route } from "react-router-dom"
+import HomePage from "./pages/home/Home.page"
+import ContactPage from "./pages/contact/Contact.page"
+import ProjectsPage from "./pages/projects/Projects.page"
 
 function App() {
 
  return (
   <>
-
-   <h1>Home</h1>
+   <Routes>
+    <Route path="/contact" element={<ContactPage />} />
+    <Route path="/projects" element={<ProjectsPage />} />
+    <Route path="/" element={<HomePage />} />
+   </Routes>
   </>
  )
 }
