4cc38bd708b1e934ee3fca362437b5f7341f081e
AdryDev <adrydevmateo@gmail.com>
2024-05-19 21:29:26 -0400

    Added projects page basic layout

--- src/pages/projects/Projects.page.tsx
+++ src/pages/projects/Projects.page.tsx
@@ -1,11 +1,76 @@
 import './Projects.style.css'
 import BaseLayout from '@layouts/Base.layout'
 
+type TProject = {
+ id: number,
+ title: string,
+ description: string,
+ year: string,
+ technologies: Array<{ name: string, link: string }>,
+ githubLink: string,
+ websiteLink: string
+}
+
 export default function ProjectsPage() {
+
+ const projects: Array<TProject> = [
+  {
+   id: 0,
+   title: 'E-commerce Website',
+   description: 'Shopping',
+   technologies: [{ name: 'react', link: 'https://react.dev/' }],
+   year: '2024',
+   githubLink: '',
+   websiteLink: ''
+  },
+  {
+   id: 0,
+   title: 'E-commerce Website',
+   description: 'Shopping',
+   technologies: [{ name: 'react', link: 'https://react.dev/' }],
+   year: '2024',
+   githubLink: '',
+   websiteLink: ''
+  }
+ ]
+
  return (
   <BaseLayout>
    <div id="projects-page">
-    <h2>Projects</h2>
+    <div className="hero">
+     <h2>Projects</h2>
+    </div>
+    <ul>
+     {projects.map(m => (
+      <li>
+       <div className="row-1">
+        <p>{m.year}</p>
+       </div>
+       <div className="row-2">
+        <h3>{m.title}</h3>
+        <p>{m.description}</p>
+       </div>
+       <div className="row-3">
+        <h4>Main Technologies</h4>
+        <div className="technologies">
+         {m.technologies.map(t => (
+          <a href={t.link} target='_blank'>
+           <strong>{t.name}</strong>
+          </a>
+         ))}
+        </div>
+       </div>
+       <div className="row-4">
+        <a href={m.githubLink} target='_blank'>
+         <strong>GitHub</strong>
+        </a>
+        <a href={m.websiteLink} target='_blank'>
+         <strong>Website</strong>
+        </a>
+       </div>
+      </li>
+     ))}
+    </ul>
    </div>
   </BaseLayout>
  )
