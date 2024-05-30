2604426bc85295360384963527b51fb79b6360b3
Adry Mateo Ramon <77759876+Adrydevmateo@users.noreply.github.com>
2024-05-30 02:00:54 +0000

    added a new project(column preview card)

--- src/pages/projects/Projects.ts
+++ src/pages/projects/Projects.ts
@@ -2,7 +2,19 @@ import { TProject } from "./Projects.types";
 
 export const projects: Array<TProject> = [
  {
-  id: 0,
+  id: crypto.randomUUID(),
+  title: 'Column Preview',
+  description: 'Frontend mentor challenge',
+  technologies: [
+   { name: 'HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
+   { name: 'CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
+  ],
+  year: '2024',
+  githubLink: 'https://github.com/Adrydevmateo/3-column-preview-card-component-main',
+  websiteLink: 'https://adrydev-column-preview-card.netlify.app/'
+ },
+ {
+  id: crypto.randomUUID(),
   title: 'Notes',
   description: 'Designed to keep your ideas organized.',
   technologies: [
@@ -17,7 +29,7 @@ export const projects: Array<TProject> = [
   websiteLink: 'https://notes-adrydev.netlify.app/'
  },
  {
-  id: 1,
+  id: crypto.randomUUID(),
   title: 'Match the Pokemon',
   description: 'Test your memory by matching the pokemons',
   technologies: [
@@ -32,7 +44,7 @@ export const projects: Array<TProject> = [
   websiteLink: 'https://adrydev-match-the-pokemon.netlify.app/'
  },
  {
-  id: 2,
+  id: crypto.randomUUID(),
   title: 'To-Do',
   description: 'Write down your tasks for today',
   technologies: [
@@ -45,7 +57,7 @@ export const projects: Array<TProject> = [
   websiteLink: 'https://adrydev-todo-app.netlify.app/'
  },
  {
-  id: 3,
+  id: crypto.randomUUID(),
   title: 'Social Links Profile',
   description: 'Frontend mentor challenge',
   technologies: [
@@ -57,7 +69,7 @@ export const projects: Array<TProject> = [
   websiteLink: 'https://adrydev-social-links-profile.netlify.app/'
  },
  {
-  id: 4,
+  id: crypto.randomUUID(),
   title: 'Newsletter Sign-Up Form',
   description: 'Frontend mentor challenge',
   technologies: [
@@ -70,7 +82,7 @@ export const projects: Array<TProject> = [
   websiteLink: 'https://adrydev-newsletter-sign-up-form.netlify.app/'
  },
  {
-  id: 5,
+  id: crypto.randomUUID(),
   title: 'Product Preview',
   description: 'Frontend mentor challenge',
   technologies: [
@@ -82,7 +94,7 @@ export const projects: Array<TProject> = [
   websiteLink: 'https://adrydev-product-preview.netlify.app/'
  },
  {
-  id: 6,
+  id: crypto.randomUUID(),
   title: 'Stats Preview Card',
   description: 'Frontend mentor challenge',
   technologies: [
@@ -94,7 +106,7 @@ export const projects: Array<TProject> = [
   websiteLink: 'https://adrydev-stats-preview-card.netlify.app/'
  },
  {
-  id: 7,
+  id: crypto.randomUUID(),
   title: 'NFT Preview Card',
   description: 'Frontend mentor challenge',
   technologies: [
@@ -106,7 +118,7 @@ export const projects: Array<TProject> = [
   websiteLink: 'https://adrydev-nft-preview-card.netlify.app/'
  },
  {
-  id: 8,
+  id: crypto.randomUUID(),
   title: 'Advice Generator',
   description: 'Frontend mentor challenge',
   technologies: [
@@ -119,7 +131,7 @@ export const projects: Array<TProject> = [
   websiteLink: 'https://adrydev-advice-generator.netlify.app/'
  },
  {
-  id: 9,
+  id: crypto.randomUUID(),
   title: 'Recipe Page',
   description: 'Frontend mentor challenge',
   technologies: [
--- src/pages/projects/Projects.types.ts
+++ src/pages/projects/Projects.types.ts
@@ -1,5 +1,5 @@
 export type TProject = {
- id: number,
+ id: string,
  title: string,
  description: string,
  year: string,
